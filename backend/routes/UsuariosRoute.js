8396a529b84d215481498b155ac64473aaa38533
Luigi Breda <luigimatheus@hotmail.com>
2024-05-09 23:18:57 -0300

    Rotas Usuarios melhoras, rota /eu adicionada, traduzindo rotas

---
+++ backend/routes/UsuariosRoute.js
@@ -0,0 +1,26 @@
+import express from "express"
+import {
+  obterTodosUsuarios,
+  obterUsuarioPorId,
+  entrar,
+  sair,
+  registrar,
+  atualizarUsuario,
+  usuarioLogado
+} from "../controller/UserController.js"
+import { verifyToken as verificaToken } from "../middlewares/verifyToken.js"
+import { refreshToken as atualizarToken } from "../controller/refreshToken.js"
+
+const router = express.Router()
+
+router.get("/usuarios", obterTodosUsuarios)
+router.get("/usuarios/:usuarioId", obterUsuarioPorId)
+router.put("/usuarios/:usuarioId", verificaToken, atualizarUsuario)
+
+router.get("/auth/eu", verificaToken, usuarioLogado);
+router.post("/auth/registrar", registrar)
+router.post("/auth/entrar", entrar)
+router.delete("/auth/sair", sair)
+router.get("/token", atualizarToken)
+
+export default router
