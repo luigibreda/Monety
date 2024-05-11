85a7710273488cfffe8dc523cb09684992897782
Luigi Breda <luigimatheus@hotmail.com>
2024-05-10 21:17:30 -0300

    Correção login

--- backend/controller/UsuariosController.js
+++ backend/controller/UsuariosController.js
@@ -144,7 +144,7 @@ export const entrar = async (req, res) => {
       maxAge: 24 * 60 * 60 * 1000
     })
 
-    res.status(200).json({ user })
+    res.status(200).json({ token })
   } catch (error) {
     console.log(error)
   }
