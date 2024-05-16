9e93be657b41fd111eb94475686508c24d0b4b10
Luigi Breda <luigimatheus@hotmail.com>
2024-05-16 15:24:32 -0300

    Cache no front no sessionStorage adicionado com tempo de vida para a ArquivosService

--- src/app/services/arquivo.service.ts
+++ src/app/services/arquivo.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs'; // Importar throwError de 'rxjs'
+import { catchError, shareReplay } from 'rxjs/operators'; // Importar catchError e shareReplay de 'rxjs/operators'
 import { environment } from '../../environments/environment';
 import { Arquivo } from '../types/arquivo.type';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +12,8 @@ import { Arquivo } from '../types/arquivo.type';
 export class ArquivosService {
 
   private apiUrl = `${environment.apiUrl}/arquivos`;
+
+  private cacheArquivos: Map<string, Observable<any>> = new Map<string, Observable<any>>();
 
   constructor(private http: HttpClient) { }
 
@@ -19,20 +23,67 @@ export class ArquivosService {
   }
 
   getAllArquivos(page: number = 0, limit: number = 10, search: string = ""): Observable<any> {
+    const cacheKey = `getAllArquivos:${page}:${limit}:${search}`;
+    const cachedData = localStorage.getItem(cacheKey);
+    const cachedTimestamp = localStorage.getItem(`${cacheKey}_timestamp`);
+
+    if (cachedData && cachedTimestamp) {
+      const expirationTime = Number(cachedTimestamp) + (1 * 6 * 100); // Definindo o tempo de vida do cache 
+      const currentTime = new Date().getTime();
+
+      if (currentTime < expirationTime) {
+        // Retornar dados em cache
+        return of(JSON.parse(cachedData));
+      }
+    }
+
     const params = new HttpParams()
       .set('page', page.toString())
       .set('limit', limit.toString())
       .set('search_query', search);
-    return this.http.get<any>(this.apiUrl, { params, headers: this.getHeaders() });
+
+    return this.http.get<any>(this.apiUrl, { params, headers: this.getHeaders() }).pipe(
+      catchError(error => throwError(error)),
+      tap(data => {
+        // Armazenar dados e timestamp no localStorage
+        localStorage.setItem(cacheKey, JSON.stringify(data));
+        localStorage.setItem(`${cacheKey}_timestamp`, new Date().getTime().toString());
+      }),
+      shareReplay(25)
+    );
   }
 
   getArquivo(id: string, page: number = 0, limit: number = 10, search: string = ""): Observable<any> {
+    const cacheKey = `getArquivo:${id}:${page}:${limit}:${search}`;
+    const cachedData = localStorage.getItem(cacheKey);
+    const cachedTimestamp = localStorage.getItem(`${cacheKey}_timestamp`);
+  
+    if (cachedData && cachedTimestamp) {
+      const expirationTime = Number(cachedTimestamp) + (1 * 60 * 1000); // Definindo o tempo de vida do cache 
+      const currentTime = new Date().getTime();
+  
+      if (currentTime < expirationTime) {
+        // Retornar dados em cache
+        return of(JSON.parse(cachedData));
+      }
+    }
+  
     const params = new HttpParams()
       .set('page', page.toString())
       .set('limit', limit.toString())
       .set('search_query', search);
-    return this.http.get<any>(`${this.apiUrl}/${id}`, { params, headers: this.getHeaders() });
+  
+    return this.http.get<any>(`${this.apiUrl}/${id}`, { params, headers: this.getHeaders() }).pipe(
+      catchError(error => throwError(error)),
+      tap(data => {
+        // Armazenar dados e timestamp no localStorage
+        localStorage.setItem(cacheKey, JSON.stringify(data));
+        localStorage.setItem(`${cacheKey}_timestamp`, new Date().getTime().toString());
+      }),
+      shareReplay(25)
+    );
   }
+
 
   uploadArquivo(form: any): Observable<any> {
     return this.http.post(`${this.apiUrl}/upload`, form, { headers: this.getHeaders(), withCredentials: true });
