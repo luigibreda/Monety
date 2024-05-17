889d5919df1377e48d5bb3672e92c20ebaa75ad5
Luigi Breda <luigimatheus@hotmail.com>
2024-05-16 21:20:24 -0300

    Mudança CORS para testar backend

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,25 @@ dotenv.config()
 
 const app = express()
 
+// Lista de domínios permitidos
+const allowedOrigins = [
+  "https://monety.vercel.app",
+  "https://monetyapp.com.br",
+  "http://localhost:4200"
+];
+
 // Middlewares
 app.use(cors({
-  origin: true,
+  origin: (origin, callback) => {
+    // Permite requisições sem origem (como as feitas por ferramentas de teste e scripts locais)
+    if (!origin) return callback(null, true);
+    // Verifica se a origem da requisição está na lista de domínios permitidos
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"] // Permitindo todos os métodos necessários
 }))
